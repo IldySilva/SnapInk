3954a5d21c4d27171e4e24c68c3d02b8e2a43356
ildeberto <ildeberto34@gmail.com>
2024-10-30 22:09:04 +0100

    addd window title

--- lib/app/components/code_editor/window_header/window_header.dart
+++ lib/app/components/code_editor/window_header/window_header.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:print_script/app/app_controller.dart';
 import 'package:print_script/app/components/code_editor/window_header/components/colored_circle_buttons.dart';
+import 'package:print_script/app/enums/editor_themes.dart';
 
 class WindowHeader extends StatelessWidget {
   const WindowHeader({super.key});
@@ -11,14 +13,27 @@ class WindowHeader extends StatelessWidget {
       width: double.infinity,
       height: windowSize,
       color: Colors.black.withOpacity(0.09),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            ColoredCircleButtons(),
-          ],
-        ),
+      child: Flex(
+        direction: Axis.horizontal,
+        children: [
+          Flexible(
+              child: Padding(
+                padding: const EdgeInsets.only(left:20.0),
+                child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: ColoredCircleButtons()),
+              )),
+          Flexible(
+              child: Align(
+                  alignment: Alignment.center,
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.grey,fontSize: 12),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,hintText: "Untitled-1"),))),
+          Flexible(
+              child:SizedBox.expand())
+        ],
       ),
     );
   }
