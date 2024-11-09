11f2a2828117e2a484fb32cfac98d74a3c818948
maksklimko <maks.klmqaz@gmail.com>
2024-11-10 01:31:52 +0200

    added drop file feature

--- lib/app/app_home.dart
+++ lib/app/app_home.dart
@@ -1,5 +1,7 @@
+import 'package:desktop_drop/desktop_drop.dart';
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
+import 'package:print_script/app/app_controller.dart';
 import 'package:print_script/app/utils/file_name_generator.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
@@ -34,47 +36,56 @@ class _HomePageState extends State<HomePage> {
             ),
             Expanded(
               flex: 6,
-              child: Container(
-                color: Theme.of(context).colorScheme.secondaryContainer,
-                height: double.infinity,
-                width: double.infinity,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                        child: Container(
-                      child: Center(
-                        child: SingleChildScrollView(
-                            child: CodeEditor(
-                          key: mainContainerKey,
-                        )),
-                      ),
-                    )),
-                    Container(
-                      padding: EdgeInsets.all(16),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          TextButton.icon(
-                            label: const Text(
-                              "Made by ildeberto",
-                            ),
-                            icon: const Icon(FontAwesome.fire_solid),
-                            onPressed: () => launchDevProfile(),
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          TextButton.icon(
+              child: DropTarget(
+                onDragDone: (details) async {
+                  //TODO: filter media files
+                  if (details.files.isNotEmpty) {
+                    final code = await details.files.first.readAsString();
+                    Controller.codeController.setCode(code);
+                  }
+                },
+                child: Container(
+                  color: Theme.of(context).colorScheme.secondaryContainer,
+                  height: double.infinity,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                          child: Container(
+                        child: Center(
+                          child: SingleChildScrollView(
+                              child: CodeEditor(
+                            key: mainContainerKey,
+                          )),
+                        ),
+                      )),
+                      Container(
+                        padding: EdgeInsets.all(16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextButton.icon(
                               label: const Text(
-                                "Github",
+                                "Made by ildeberto",
                               ),
-                              icon: const Icon(FontAwesome.github_alt_brand),
-                              onPressed: () => launchGithub()),
-                        ],
-                      ),
-                    )
-                  ],
+                              icon: const Icon(FontAwesome.fire_solid),
+                              onPressed: () => launchDevProfile(),
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            TextButton.icon(
+                                label: const Text(
+                                  "Github",
+                                ),
+                                icon: const Icon(FontAwesome.github_alt_brand),
+                                onPressed: () => launchGithub()),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
